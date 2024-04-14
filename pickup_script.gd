d9072ba21e18dcb95a160b7d0a0fa16b91a82461
lawenhunar <lawahunar22@gmail.com>
2024-04-14 20:42:48 +0300

    removing this commit

--- pickup_script.gd
+++ pickup_script.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
 @export var item: InvItem
+@export var item_name: String
+@onready var sprite_2d = $interactible_area/Sprite2D
 
 var player=null
 var player_in_area=false
 
 func _ready():
+	#sprite_2d.texture
 	pass
 
 
@@ -17,18 +20,3 @@ func _on_interactible_area_body_entered(body):
 		player.collect(item)
 		await get_tree().create_timer(0.1).timeout
 		self.queue_free()
-
-
-
-#func _on_interactible_area_body_exited(body):
-	#print("entered area")
-	#if body.has_method("Player"):
-		#player=body
-		#player_in_area=true
-		#player.collect(item)
-		#await get_tree().create_timer(0.2).timeout
-		#self.queue_free()
-
-
-
-
