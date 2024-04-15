20074307da1d4fac1dbdabac3adf7c56216e4625
dostee-hemin <dostee.hemin@gmail.com>
2024-04-15 11:11:11 +0300

    Changed TextureButton to Button + Changed GridContainer to HBoxContainer
    Previously, the alignment of our character setup nodes in the grid container wasn't properly done. Some characters would get cut off, sometimes the scroll won't cover all characters, and it wasn't scalable.
    
    Now, I have made some changes to have the setup nodes properly laid out. Here are all the changes:
    
    - The previous GridContainer is now an HBoxContainer and is called "Slider", since we don't need to scroll downwards, we can just scroll horizontally.
    
    - The Setup_Scene.gd script no longer exports the grid separation values or column number because they are not needed.
    
    - The TextureButton that we previously used for the Character_Setup.tscn scene didn't have any hover or pressed visual features, so I changed it to a regular Button and set the icon to the appropriate agent
    
    - The Container node in the Character_Setup.tscn scene was simply a point in 2D space, which is what caused the wrong alignment and clipping oreviously. Now, the root node is a MarginContainer node, which allows us to set the spacing between each character setup node in the slider.

--- Scripts/Setup_Scene.gd
+++ Scripts/Setup_Scene.gd
@@ -1,40 +1,39 @@
 extends Node2D
 
-@onready var character_grid : GridContainer = $"../CanvasLayer/ScrollContainer/GridContainer"
+@onready var slider : HBoxContainer = $"../CanvasLayer/ScrollContainer/Slider"
 @onready var character_setup = preload("res://Resource_Scenes/Character_Setup.tscn")
-
-@export var grid_columns : int = 3
-@export var grid_h_spacing : int = 400
-@export var grid_v_spacing : int = 300
-
-var frame_size = Vector2(16, 32) # Size of each frame
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# Current index
 	var i : int = 0
+	# Folder where agent image sheets are located
 	var folder_path : String = "res://Assets/Graphics/32x32/"
 	while true:
+		# Continue the loop only if the current agent image sheet exists
 		var current_path : String = folder_path+"Agent_"+str(i)+".png"
 		if !FileAccess.file_exists(current_path):
 			break
 		
+		# Get the icon of the agent from the image sheet
 		var agent_image = load(current_path).get_image()
 		var icon_image = _get_image_region(0, 3, agent_image)
 		var new_size = icon_image.get_size()*10
 		icon_image.resize(new_size.x,new_size.y,0)
 		
+		# Create a new character setup node by setting all its respective properties and adding it to the grid
 		var new_setup = character_setup.instantiate()
-		new_setup.find_child("Sprite Button").texture_normal = ImageTexture.create_from_image(icon_image)
+		new_setup.find_child("Sprite Button").icon = ImageTexture.create_from_image(icon_image)
 		new_setup.find_child("Name Label").text = ""
 		new_setup.find_child("History Label").text = ""
-		character_grid.add_child(new_setup)
+		slider.add_child(new_setup)
 		
 		i += 1
-	character_grid.columns = grid_columns
-	character_grid.set("theme_override_constants/h_separation", grid_h_spacing)
-	character_grid.set("theme_override_constants/v_separation", grid_v_spacing)
 
+# Get a specific frame from the image sheet at a specific row and column, and make sure you cut out that extra space at the top
 func _get_image_region(row, column, image) -> Image:
-	var frame_position = Vector2(column * frame_size.x, row * frame_size.y)
-	var region = Rect2i(frame_position, frame_size)
+	var frame_size = Vector2(16, 32) # Size of each frame
+	var frame_position = Vector2(column * frame_size.x, (row+0.3) * frame_size.y)
+	var cutout_size = Vector2(frame_size.x,frame_size.y*0.7)
+	var region = Rect2i(frame_position, cutout_size)
 	return image.get_region(region)
