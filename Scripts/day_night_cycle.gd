a150b336b68a05de9f4b6566339fbab8463a0c27
dostee-hemin <dostee.hemin@gmail.com>
2024-04-29 23:50:50 +0300

    Added Smoother Transition between Day/Night Colors
    Previously, the day/night colors were picked based on the integer value of the current hour. This worked, but because a lot can change in an hour, you would see a drastic change in the color between, say, 8:59 am and 9:00 am.
    
    Now, the color is picked based on a floating point value that combines the current hour and minute to generate a value between 0.0 - 24.0, leading to smoother transitions.

--- Scripts/day_night_cycle.gd
+++ Scripts/day_night_cycle.gd
@@ -8,22 +8,21 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta:float):
-	var time1=game_manager.get_current_datetime_string()
-	var time2=get_hour_from_time(time1)
-	#print(time2)
+	var time_as_float = _convert_time_to_float(game_manager.get_current_datetime_string())
 	# Adjust the time to start from 0 at 8 pm (20:00)
-	var adjusted_time = (time2 - 8) % 24  # Ensure the result stays within 0-23
+	var adjusted_time = (time_as_float - 8)
 	# Calculate the phase based on adjusted time
 	var phase = (adjusted_time / 24.0) * 2 * PI  # Convert time to radians
 	var value=(sin(phase)+1.0)/2
 	self.color=gradient.gradient.sample(value)
 
-func get_hour_from_time(time_str: String) -> int:
+func _convert_time_to_float(time_str: String) -> float:
 	# Split the time string by colon (:) to separate hours, minutes, and seconds
 	var date_parts := time_str.split(",")
-	var time_parts :=date_parts[2].split(":")
+	var time_parts := date_parts[2].split(":")
 	# Get the hour part (index 0 after splitting)
-	var hour_str := time_parts[0].trim_prefix(" ") # Remove leading/trailing spaces
-	# Convert the hour string to an integer
-	var hour := hour_str.to_int()
-	return hour
+	var hour_str := time_parts[0].strip_edges()
+	var minute_str = time_parts[1].strip_edges()
+	# Convert the time string to a floating point value from 0.0 - 24.0
+	var time_as_float = float(hour_str) + float(minute_str)/60
+	return time_as_float
