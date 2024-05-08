ad0bb5484464a26ce596d258b0f34b3dfeaf815d
dostee-hemin <dostee.hemin@gmail.com>
2024-05-09 02:28:58 +0300

    Replaced "Talk Button" with "ENTER" Key
    Previously, the player would have to move the mouse over to the talk button so that the text can be sent to the agents for conversing.
    
    Now, to make it easier to talk, the player can simply hit enter.

--- Scripts/Dialogue_Panel.gd
+++ Scripts/Dialogue_Panel.gd
@@ -6,21 +6,15 @@ extends Panel
 @onready var agent_icon = $"Agent Icon"
 @onready var agent_name = $"Agent Name"
 @onready var player_input_box = $"Player Input Box"
-@onready var talk_button = $"Talk Button"
 @onready var leave_button = $"Leave Button"
+
+var can_talk : bool
 
 func initialize_with_agent(agent):
 	agent_icon.texture = agent.icon
 	agent_name.text = agent.agent_name
 	dialogue_text.text = "..."
-	talk_button.disabled = true
-
-
-func _on_talk_button_pressed():
-	game_manager.send_dialogue_text(player_input_box.text)
-	player_input_box.text = ""
-	dialogue_text.text = "..."
-	talk_button.disabled = true
+	can_talk = false
 
 
 func _on_leave_button_pressed():
@@ -28,7 +22,14 @@ func _on_leave_button_pressed():
 	
 func on_agent_talk(agent_dialogue):
 	dialogue_text.text = agent_dialogue
-	talk_button.disabled = false
-	
+	can_talk = true
 
+func _on_player_input_box_text_changed():
+	if !can_talk:
+		return
 
+	if "\n" in player_input_box.text and player_input_box.text.strip_edges().strip_escapes() != "":
+		game_manager.send_dialogue_text(player_input_box.text)
+		player_input_box.text = ""
+		dialogue_text.text = "..."
+		can_talk = false
